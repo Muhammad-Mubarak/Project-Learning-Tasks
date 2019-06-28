78b576aaf40f0b85bb309705a062ce86ad020c1b
jaweria_javaid <jaweriajavaid1998@gmail.com>
2019-06-27 21:49:49 -0700

    My task

---
+++ Jaweria/Company/Company/Data/employeeContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.Models
+{
+    public class employeeContext : DbContext
+    {
+        public employeeContext (DbContextOptions<employeeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Company.Models.emp> emp { get; set; }
+    }
+}
